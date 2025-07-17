4f113314bc9d390267b9e6ed0b50a53f7e2673a5
Yll Kryeziu <yll.kryeziu@tum.de>
2025-07-17 13:21:21 +0200

    Correct configs

--- train.sh
+++ train.sh
@@ -2,11 +2,13 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=students
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=10G
 #SBATCH --output=.slurm_outputs/slurm_%A.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yll.kryeziu@tum..de
 
 echo "Starting ..."
-autrainer train -cn config-balanced.yaml
+autrainer train -cn config-fixed.yaml
 echo "Job finished."
 
